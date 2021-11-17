cad46be893a8a19785f288f1b48107d0b60b348f
Raul <raulsiemar@gmail.com>
2021-11-17 18:57:09 +0100

    second commit

---
+++ part1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Mensaje from './Mensaje';
+
+function App() {
+  return (
+    <div className="App">
+      <Mensaje msj='Estamos'/>
+      <Mensaje msj='en'/>
+      <Mensaje msj='un curso'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ part1/src/Mensaje.js
@@ -0,0 +1,5 @@
+const Mensaje = (props) => {
+    return <h1>{props.msj}</h1>
+  }
+
+export default Mensaje
